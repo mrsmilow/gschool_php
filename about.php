9c7acc0fe000c9784d7249f6de54cd20f899b111
mrsmilow <mr.stanlonmilow@gmail.com>
2015-07-26 17:10:13 -0600

    PHP and Sass Conversion

--- about.php
+++ about.php
@@ -16,22 +16,6 @@
       </div>
     </article> 
 
-    <!-- Sidebar -->
-    <sidebar class="column one-third about_sidebar">
-      <div class="row clearfix">
-        <h2>Widget Headline</h2>
-        <p>
-          Haircut engaged something Dibble Dibble Dibble Dibble nonsensical alone sadly prickle- ly playthings Wickersham Brothers guaranteed murmur.
-        </p>
-        <h2>Widget Headline</h2>
-        <p>
-          Haircut engaged something Dibble Dibble Dibble Dibble nonsensical alone sadly prickle- ly playthings Wickersham Brothers guaranteed murmur.
-        </p>
-        <h2>Widget Headline</h2>
-        <p>
-          Haircut engaged something Dibble Dibble Dibble Dibble nonsensical alone sadly prickle- ly playthings Wickersham Brothers guaranteed murmur.
-        </p>
-      </div>
-    </sidebar>   
+    <?php include "sidebar.php" ?>    
 
    <?php include "footer.php" ?>
---
+++ sidebar.php
@@ -0,0 +1,17 @@
+<!-- Sidebar -->
+    <sidebar class="column one-third about_sidebar">
+      <div class="row clearfix">
+        <h2>Widget Headline</h2>
+        <p>
+          Haircut engaged something Dibble Dibble Dibble Dibble nonsensical alone sadly prickle- ly playthings Wickersham Brothers guaranteed murmur.
+        </p>
+        <h2>Widget Headline</h2>
+        <p>
+          Haircut engaged something Dibble Dibble Dibble Dibble nonsensical alone sadly prickle- ly playthings Wickersham Brothers guaranteed murmur.
+        </p>
+        <h2>Widget Headline</h2>
+        <p>
+          Haircut engaged something Dibble Dibble Dibble Dibble nonsensical alone sadly prickle- ly playthings Wickersham Brothers guaranteed murmur.
+        </p>
+      </div>
+    </sidebar>  
